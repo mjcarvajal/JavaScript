57db4a94ac20f741a0401edb2723b3c62ec55c1e
mjcarvajal <maryurycarvajalcamargo@gmail.com>
2020-02-20 21:00:57 -0500

    Se añaden cambios iniciales para el cambio de temas

--- ProyectoJs/js/main.js
+++ ProyectoJs/js/main.js
@@ -34,6 +34,11 @@ $(document).ready(function(){
             date: 'Publicado el dia: '+moment().day()+' de '+moment().format("MMMM")+' Del '+moment().format("YYYY"),
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum id diam eget tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec et magna luctus, scelerisque erat ut, ultrices justo. Nam non imperdiet nulla. Nulla diam lorem, eleifend nec dolor quis, rutrum facilisis ante. Vivamus nec arcu et arcu venenatis condimentum. Pellentesque lobortis, sem eu mattis porttitor, ligula ligula iaculis augue, at bibendum orci turpis vitae nunc. Vivamus fringilla at massa quis tincidunt. Duis euismod nunc ac nisi mattis placerat. Ut lacinia hendrerit nibh et aliquam. Quisque eget bibendum diam, nec dapibus nunc."
         },
+        {
+            title: "Prueba de titulo 6",
+            date: 'Publicado el dia: '+moment().day()+' de '+moment().format("MMMM")+' Del '+moment().format("YYYY"),
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum id diam eget tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec et magna luctus, scelerisque erat ut, ultrices justo. Nam non imperdiet nulla. Nulla diam lorem, eleifend nec dolor quis, rutrum facilisis ante. Vivamus nec arcu et arcu venenatis condimentum. Pellentesque lobortis, sem eu mattis porttitor, ligula ligula iaculis augue, at bibendum orci turpis vitae nunc. Vivamus fringilla at massa quis tincidunt. Duis euismod nunc ac nisi mattis placerat. Ut lacinia hendrerit nibh et aliquam. Quisque eget bibendum diam, nec dapibus nunc."
+        },
     ];
 
     posts.forEach((item, index) =>{
@@ -45,14 +50,6 @@ $(document).ready(function(){
                         <a href="#" class="button-more">Leer más</a>
                     </article>
         `;
+        $("#posts").append(post);
     });
-    /*
-    <article class="post">
-                        <h2>Prueba de titulo</h2>
-                        <span>Fecha de publicación</span>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum id diam eget tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec et magna luctus, scelerisque erat ut, ultrices justo. Nam non imperdiet nulla. Nulla diam lorem, eleifend nec dolor quis, rutrum facilisis ante. Vivamus nec arcu et arcu venenatis condimentum. Pellentesque lobortis, sem eu mattis porttitor, ligula ligula iaculis augue, at bibendum orci turpis vitae nunc. Vivamus fringilla at massa quis tincidunt. Duis euismod nunc ac nisi mattis placerat. Ut lacinia hendrerit nibh et aliquam. Quisque eget bibendum diam, nec dapibus nunc.
-                        </p>
-                        <a href="#" class="button-more">Leer más</a>
-                    </article>*/
 });
