66c8dd73009033dac5b30e2f4fc2e72ff25ed539
mjcarvajal <maryurycarvajalcamargo@gmail.com>
2020-02-24 15:55:30 -0500

    Se añaden los Cambios relacionados a el cambio de temas y el scroll de la pagina

--- ProyectoJs/js/main.js
+++ ProyectoJs/js/main.js
@@ -52,4 +52,26 @@ $(document).ready(function(){
         `;
         $("#posts").append(post);
     });
+
+    // Selector tema 
+    var theme = $("#theme");
+    $("#to-green").click(function(){
+        theme.attr("href","css/green.css");
+    });
+    $("#to-red").click(function(){
+        theme.attr("href","css/red.css");
+    });
+    $("#to-blue").click(function(){
+        theme.attr("href","css/blue.css");
+    });
+
+    // Scroll Arriba
+
+    $(".subir").clicl(function(forEach){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 });
