e6c902280b9ef92744b93b1ef43a1b93c2c2e9f8
mjcarvajal <maryurycarvajalcamargo@gmail.com>
2020-02-26 08:01:30 -0500

    Se Añade la validación de formularios con un plugin de JQuery

--- ProyectoJs/js/main.js
+++ ProyectoJs/js/main.js
@@ -110,7 +110,17 @@ $(document).ready(function(){
             var reloj = moment().format('hh:mm:ss');
             $('#reloj').html(reloj);
         },1000);
-
     }
 
+    // Validacion
+    if(window.location.href.indexOf('contact') > -1){
+        $("form input[name='date']").datepicker({
+            dateFormat: 'dd/mm/yy'
+        });
+        $.validate({
+            lang: 'es',
+            errorMessagePosition:"top",
+            scrollToTopOnError: true
+        });
+    }
 });
